96533af1fa9b088deeaeacc8b882e150f3b678ee
gCKn <bibasitshak@gmail.com>
2019-12-26 17:59:03 +0100

    la nouvelle classe

---
+++ githubentrainnelent/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubentrainnelent
+{
+    class Class1
+    {
+        Class1   A;
+        int a = 13;
+        string c = "va te faire voir";
+
+    }
+}
